86bba6e031c6ab32754bfb8d1fdbdf812efb7e48
leekyukin <leekujin14@gmail.com>
2022-11-16 20:15:23 +0900

    fix: not implemented

--- src/main/kotlin/com/sexysisters/tojserverv2/infrastructure/NotificationExecutor.kt
+++ src/main/kotlin/com/sexysisters/tojserverv2/infrastructure/NotificationExecutor.kt
@@ -7,17 +7,17 @@ import org.springframework.stereotype.Component
 class NotificationExecutor: NotificationService {
 
     override fun sendEmail(email: String, title: String, description: String) {
-        TODO("이메일 발송 구현")
+//        TODO("이메일 발송 구현")
         println("sendEmail")
     }
 
     override fun sendKakao(phoneNo: String, description: String) {
-        TODO("카카오톡 메시지 전송 구현")
+//        TODO("카카오톡 메시지 전송 구현")
         println("sendKakao")
     }
 
     override fun sendSms(phoneNo: String, description: String) {
-        TODO("SMS 발송 구현")
+//        TODO("SMS 발송 구현")
         println("sendSms")
     }
 }
