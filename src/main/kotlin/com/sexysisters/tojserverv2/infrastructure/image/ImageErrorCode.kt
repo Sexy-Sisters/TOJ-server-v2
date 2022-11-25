902cde7b383706743251cf778196584a5e2937ae
leekyukin <leekujin14@gmail.com>
2022-11-25 21:55:48 +0900

    add: image upload exceptions

---
+++ src/main/kotlin/com/sexysisters/tojserverv2/infrastructure/image/ImageErrorCode.kt
@@ -0,0 +1,11 @@
+package com.sexysisters.tojserverv2.infrastructure.image
+
+import com.sexysisters.tojserverv2.common.response.ErrorProperty
+
+enum class ImageErrorCode(
+    override val errorMsg: String,
+) : ErrorProperty {
+    FAILED_SAVE_FILE("Faild to save file"),
+    EMPTY_FILE("File is empty"),
+    TOO_LONG_TITLE("Title is too long"),
+}
