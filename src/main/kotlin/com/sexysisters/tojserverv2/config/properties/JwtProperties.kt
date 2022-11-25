62bd743f59c914b7b707486eba1294b8fdd52f44
leekyukin <leekujin14@gmail.com>
2022-11-25 20:26:02 +0900

    add: validation

--- src/main/kotlin/com/sexysisters/tojserverv2/config/properties/JwtProperties.kt
+++ src/main/kotlin/com/sexysisters/tojserverv2/config/properties/JwtProperties.kt
@@ -2,12 +2,18 @@ package com.sexysisters.tojserverv2.config.properties
 
 import org.springframework.boot.context.properties.ConfigurationProperties
 import org.springframework.boot.context.properties.ConstructorBinding
+import org.springframework.validation.annotation.Validated
+import javax.validation.constraints.NotEmpty
 
 @ConstructorBinding
+@Validated
 @ConfigurationProperties(prefix = "spring.jwt")
 data class JwtProperties(
+    @NotEmpty
     val accessTokenValidTime: Long,
+    @NotEmpty
     val refreshTokenValidTime: Long,
+    @NotEmpty
     val secret: String,
 ) {
     val jwtPrefix = "Bearer"
