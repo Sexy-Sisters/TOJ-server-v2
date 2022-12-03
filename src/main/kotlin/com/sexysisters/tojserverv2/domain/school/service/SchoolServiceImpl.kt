476a2a33ff74289eae111e4bcbc58e47299dc5b6
leekyukin <leekujin14@gmail.com>
2022-12-04 00:27:16 +0900

    feat: (#39) create school

--- src/main/kotlin/com/sexysisters/tojserverv2/domain/school/service/SchoolServiceImpl.kt
+++ src/main/kotlin/com/sexysisters/tojserverv2/domain/school/service/SchoolServiceImpl.kt
@@ -5,15 +5,19 @@ import com.sexysisters.tojserverv2.domain.school.SchoolInfo
 import com.sexysisters.tojserverv2.domain.school.SchoolMapper
 import com.sexysisters.tojserverv2.domain.school.design.SchoolReader
 import com.sexysisters.tojserverv2.domain.school.design.SchoolStore
-import com.sexysisters.tojserverv2.domain.user.setNone
+import com.sexysisters.tojserverv2.domain.school.exception.SchoolExcpetion
+import com.sexysisters.tojserverv2.domain.user.design.UserReader
+import com.sexysisters.tojserverv2.domain.user.setEngaged
 import com.sexysisters.tojserverv2.domain.user.setRelation
-import com.sexysisters.tojserverv2.infrastructure.neis.SchoolInfoReader
+import com.sexysisters.tojserverv2.domain.user.setWaiting
+import com.sexysisters.tojserverv2.domain.user.type.ApplyStatus
+import com.sexysisters.tojserverv2.infrastructure.neis.NeisSchoolReader
 import org.springframework.stereotype.Service
 import org.springframework.transaction.annotation.Transactional
 
 @Service
 class SchoolServiceImpl(
-    private val schoolInfoReader: SchoolInfoReader,
+    private val neisSchoolReader: NeisSchoolReader,
     private val schoolMapper: SchoolMapper,
     private val schoolReader: SchoolReader,
     private val schoolStore: SchoolStore,
@@ -32,6 +36,9 @@ class SchoolServiceImpl(
 
     @Transactional
     override fun createSchool(code: String) {
+        val school = neisSchoolReader.findSchoolByCode(code)
+        schoolStore.store(school)
+    }
         val school = schoolReader.findSchoolByCode(code)
         schoolStore.store(school)
     }
