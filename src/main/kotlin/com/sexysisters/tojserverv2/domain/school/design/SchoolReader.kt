015750fa2f33fceb477a2e16f1a986e78d2ef5c6
leekyukin <leekujin14@gmail.com>
2022-12-05 19:38:23 +0900

    refactor: follow the naming convention

--- src/main/kotlin/com/sexysisters/tojserverv2/domain/school/design/SchoolReader.kt
+++ src/main/kotlin/com/sexysisters/tojserverv2/domain/school/design/SchoolReader.kt
@@ -3,5 +3,5 @@ package com.sexysisters.tojserverv2.domain.school.design
 import com.sexysisters.tojserverv2.domain.school.School
 
 interface SchoolReader {
-    fun findSchoolByCode(code: String): School
+    fun getSchool(code: String): School
 }
--- src/main/kotlin/com/sexysisters/tojserverv2/infrastructure/school/student/StudentRepository.kt
+++ src/main/kotlin/com/sexysisters/tojserverv2/infrastructure/school/student/StudentRepository.kt
@@ -3,5 +3,4 @@ package com.sexysisters.tojserverv2.infrastructure.school.student
 import com.sexysisters.tojserverv2.domain.school.student.Student
 import org.springframework.data.jpa.repository.JpaRepository
 
-interface StudentRepository : JpaRepository<Student, Long> {
-}
+interface StudentRepository : JpaRepository<Student, Long>
