a67ca2e0065384b746c85c331efe2ee952bf2930
leekyukin <leekujin14@gmail.com>
2022-12-10 09:28:01 +0900

    fix: search school (request body -> param)

--- src/main/kotlin/com/sexysisters/tojserverv2/interfaces/school/SchoolApiController.kt
+++ src/main/kotlin/com/sexysisters/tojserverv2/interfaces/school/SchoolApiController.kt
@@ -4,17 +4,14 @@ import com.sexysisters.tojserverv2.application.school.SchoolFacade
 import com.sexysisters.tojserverv2.common.response.CommonResponse
 import com.sexysisters.tojserverv2.domain.school.service.SchoolService
 import com.sexysisters.tojserverv2.interfaces.school.dto.SchoolDtoMapper
-import com.sexysisters.tojserverv2.interfaces.school.dto.SchoolRequest
 import com.sexysisters.tojserverv2.interfaces.school.dto.SchoolResponse
 import io.swagger.annotations.Api
 import io.swagger.annotations.ApiOperation
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.PostMapping
-import org.springframework.web.bind.annotation.RequestBody
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RequestParam
 import org.springframework.web.bind.annotation.RestController
-import javax.validation.Valid
 
 @Api(tags = ["School 관련 API"])
 @RestController
@@ -28,10 +25,10 @@ class SchoolApiController(
     @ApiOperation(value = "학교 검색")
     @GetMapping
     fun searchSchool(
-        @RequestBody @Valid request: SchoolRequest.Search
+        @RequestParam(name = "name") name: String,
+        @RequestParam(name = "belong") belong: String,
     ): CommonResponse<List<SchoolResponse.Search>> {
-        val schoolCommand = schoolDtoMapper.of(request)
-        val schoolInfo = schoolService.searchSchool(schoolCommand)
+        val schoolInfo = schoolService.searchSchool(name, belong)
         val response = schoolInfo.map { schoolDtoMapper.of(it) }
         return CommonResponse.success(response)
     }
