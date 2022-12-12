1551a91d3426588e337b540ffdb6c2299e94e71a
leekyukin <leekujin14@gmail.com>
2022-12-13 05:54:07 +0900

    add: @ResponseStatus(HttpStatus.CREATED)

--- src/main/kotlin/com/sexysisters/tojserverv2/interfaces/image/ImageApiController.kt
+++ src/main/kotlin/com/sexysisters/tojserverv2/interfaces/image/ImageApiController.kt
@@ -3,10 +3,12 @@ package com.sexysisters.tojserverv2.interfaces.image
 import com.sexysisters.tojserverv2.infrastructure.image.s3.Dir
 import com.sexysisters.tojserverv2.infrastructure.image.s3.S3Executor
 import com.sexysisters.tojserverv2.infrastructure.image.s3.getDirName
+import org.springframework.http.HttpStatus
 import org.springframework.web.bind.annotation.PostMapping
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RequestParam
 import org.springframework.web.bind.annotation.RequestPart
+import org.springframework.web.bind.annotation.ResponseStatus
 import org.springframework.web.bind.annotation.RestController
 import org.springframework.web.multipart.MultipartFile
 
@@ -17,6 +19,7 @@ class ImageApiController(
 ) {
 
     @PostMapping
+    @ResponseStatus(HttpStatus.CREATED)
     fun saveImage(
         @RequestPart images: List<MultipartFile>,
         @RequestParam(name = "dir")dir: Dir,
--- src/main/kotlin/com/sexysisters/tojserverv2/interfaces/student/StudentApiController.kt
+++ src/main/kotlin/com/sexysisters/tojserverv2/interfaces/student/StudentApiController.kt
@@ -7,11 +7,13 @@ import com.sexysisters.tojserverv2.interfaces.student.dto.StudentDtoMapper
 import com.sexysisters.tojserverv2.interfaces.student.dto.StudentRequest
 import com.sexysisters.tojserverv2.interfaces.student.dto.StudentResponse
 import io.swagger.annotations.ApiOperation
+import org.springframework.http.HttpStatus
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.PostMapping
 import org.springframework.web.bind.annotation.RequestBody
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RequestParam
+import org.springframework.web.bind.annotation.ResponseStatus
 import org.springframework.web.bind.annotation.RestController
 import javax.validation.Valid
 
@@ -23,6 +25,7 @@ class StudentApiController(
 ) {
 
     @ApiOperation(value = "학교에 가입할 학생 생성")
+    @ResponseStatus(HttpStatus.CREATED)
     @PostMapping
     fun createStudent(
         @RequestBody @Valid request: StudentRequest.Create
@@ -33,6 +36,7 @@ class StudentApiController(
     }
 
     @ApiOperation(value = "학생 리스트 조회")
+    @ResponseStatus(HttpStatus.CREATED)
     @GetMapping
     fun getWaitingList(@RequestParam("status") status: Status): CommonResponse<List<StudentResponse.Main>> {
         val studentInfo = studentService.getStudentList(status)
