c2e84a0546e85a7166103ff02e1faeff095c8b8f
leekyukin <leekujin14@gmail.com>
2022-12-13 11:55:38 +0900

    add: id property

--- src/main/kotlin/com/sexysisters/tojserverv2/domain/wiki/WikiInfo.kt
+++ src/main/kotlin/com/sexysisters/tojserverv2/domain/wiki/WikiInfo.kt
@@ -3,6 +3,7 @@ package com.sexysisters.tojserverv2.domain.wiki
 class WikiInfo {
 
     class Main(
+        val id: Long,
         val name: String,
         val content: String,
     )
--- src/main/kotlin/com/sexysisters/tojserverv2/interfaces/wiki/dto/WikiResponse.kt
+++ src/main/kotlin/com/sexysisters/tojserverv2/interfaces/wiki/dto/WikiResponse.kt
@@ -3,6 +3,7 @@ package com.sexysisters.tojserverv2.interfaces.wiki.dto
 class WikiResponse {
 
     data class Main(
+        val id: Long,
         val name: String,
         val content: String,
     )
