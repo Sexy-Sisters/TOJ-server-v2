677a67ca8bc5874bf29232f0a08d131bbfcdbef9
leekyukin <leekujin14@gmail.com>
2022-12-18 14:35:56 +0900

    style: change name

--- src/main/kotlin/com/sexysisters/tojserverv2/common/util/api/oauth/client/GoogleInfoClient.kt
+++ src/main/kotlin/com/sexysisters/tojserverv2/common/util/api/oauth/client/GoogleInfoClient.kt
@@ -1,6 +1,6 @@
 package com.sexysisters.tojserverv2.common.util.api.oauth.client
 
-import com.sexysisters.tojserverv2.common.util.api.oauth.dto.GoogleInfoResponse
+import com.sexysisters.tojserverv2.common.util.api.oauth.dto.OAuthInfoResponse
 import org.springframework.cloud.openfeign.FeignClient
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.PathVariable
@@ -9,5 +9,5 @@ import org.springframework.web.bind.annotation.PathVariable
 interface GoogleInfoClient {
 
     @GetMapping("?alt=json&access_token={TOKEN}")
-    fun googleInfo(@PathVariable("TOKEN") accessToken: String): GoogleInfoResponse
+    fun googleInfo(@PathVariable("TOKEN") accessToken: String): OAuthInfoResponse
 }
