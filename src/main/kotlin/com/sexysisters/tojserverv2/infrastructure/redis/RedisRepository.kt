e2c2d58fa02f870da7ce290a8be4b1e8ebb025d1
leekyukin <leekujin14@gmail.com>
2022-12-18 14:34:47 +0900

    refactor: apply DIP to Redis infrastructure

--- src/main/kotlin/com/sexysisters/tojserverv2/infrastructure/redis/RedisRepository.kt
+++ src/main/kotlin/com/sexysisters/tojserverv2/infrastructure/redis/RedisRepository.kt
@@ -1,30 +1,25 @@
 package com.sexysisters.tojserverv2.infrastructure.redis
 
-import org.springframework.data.redis.core.RedisTemplate
-import org.springframework.stereotype.Component
 import java.time.Duration
 
-@Component
-class RedisRepository(
-    private val stringTemplate: RedisTemplate<String, Any>,
-    private val blackListTemplate: RedisTemplate<String, Any>,
-) {
+interface RedisRepository {
+    fun getData(key: String): Any?
+    fun setData(
+        key: String,
+        value: String
+    )
 
-    fun getData(key: String) =
-        stringTemplate.opsForValue().get(key)
+    fun setDataExpired(
+        key: String,
+        value: String,
+        duration: Duration,
+    )
 
-    fun setData(key: String, value: String) =
-        stringTemplate.opsForValue().set(key, value)
+    fun deleteData(key: String): Boolean
+    fun setBlackList(
+        key: String,
+        duration: Duration
+    )
 
-    fun setDataExpired(key: String, value: String, duration: Duration) =
-        stringTemplate.opsForValue().set(key, value, duration)
-
-    fun deleteData(key: String) =
-        stringTemplate.delete(key)
-
-    fun setBlackList(key: String, duration: Duration) =
-        blackListTemplate.opsForValue().set(key, "LOGOUT", duration)
-
-    fun hasBlackList(key: String) =
-        blackListTemplate.hasKey(key)
+    fun hasBlackList(key: String): Boolean
 }
---
+++ src/main/kotlin/com/sexysisters/tojserverv2/infrastructure/redis/RedisRepositoryImpl.kt
@@ -0,0 +1,30 @@
+package com.sexysisters.tojserverv2.infrastructure.redis
+
+import org.springframework.data.redis.core.RedisTemplate
+import org.springframework.stereotype.Component
+import java.time.Duration
+
+@Component
+class RedisRepositoryImpl(
+    private val stringTemplate: RedisTemplate<String, Any>,
+    private val blackListTemplate: RedisTemplate<String, Any>,
+) : RedisRepository {
+
+    override fun getData(key: String) =
+        stringTemplate.opsForValue().get(key)
+
+    override fun setData(key: String, value: String) =
+        stringTemplate.opsForValue().set(key, value)
+
+    override fun setDataExpired(key: String, value: String, duration: Duration) =
+        stringTemplate.opsForValue().set(key, value, duration)
+
+    override fun deleteData(key: String) =
+        stringTemplate.delete(key)
+
+    override fun setBlackList(key: String, duration: Duration) =
+        blackListTemplate.opsForValue().set(key, "LOGOUT", duration)
+
+    override fun hasBlackList(key: String) =
+        blackListTemplate.hasKey(key)
+}
