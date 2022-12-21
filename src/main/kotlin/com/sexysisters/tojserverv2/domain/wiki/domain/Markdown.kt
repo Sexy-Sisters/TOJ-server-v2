a6dd98e272f392429b1e4345fd9431aee47e8d86
leekyukin <leekujin14@gmail.com>
2022-12-21 11:24:31 +0900

    style: remove unused imports

--- src/main/kotlin/com/sexysisters/tojserverv2/domain/wiki/domain/Markdown.kt
+++ src/main/kotlin/com/sexysisters/tojserverv2/domain/wiki/domain/Markdown.kt
@@ -1,7 +1,6 @@
 package com.sexysisters.tojserverv2.domain.wiki.domain
 
 import com.sexysisters.tojserverv2.config.properties.WikiProperties
-import com.sexysisters.tojserverv2.domain.wiki.exception.WikiException
 import javax.persistence.Column
 import javax.persistence.Embeddable
 import javax.validation.constraints.NotNull
--- src/main/kotlin/com/sexysisters/tojserverv2/domain/wiki/domain/Views.kt
+++ src/main/kotlin/com/sexysisters/tojserverv2/domain/wiki/domain/Views.kt
@@ -1,6 +1,5 @@
 package com.sexysisters.tojserverv2.domain.wiki.domain
 
-import com.sexysisters.tojserverv2.domain.wiki.exception.WikiException
 import javax.persistence.Column
 import javax.validation.constraints.NotNull
 
