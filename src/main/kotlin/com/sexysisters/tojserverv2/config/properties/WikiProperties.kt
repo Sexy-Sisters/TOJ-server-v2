a5524e1d6ca6eed2e3531263747d5bc74729dee3
leekyukin <leekujin14@gmail.com>
2022-12-21 15:12:18 +0900

    fix: wiki default value

--- src/main/kotlin/com/sexysisters/tojserverv2/config/properties/WikiProperties.kt
+++ src/main/kotlin/com/sexysisters/tojserverv2/config/properties/WikiProperties.kt
@@ -2,6 +2,11 @@ package com.sexysisters.tojserverv2.config.properties
 
 class WikiProperties {
     companion object {
-        val EMPTY = "<img src=\"https://im4.ezgif.com/tmp/ezgif-4-37852bc226.gif\">"
+        val EMPTY =
+            "<iframe " +
+                "src=${"https://assets.pinterest.com/ext/embed.html?id=704743041686828494"} " +
+                "height=${"445"} width=${"345"} " +
+                "frameborder=${"0"} scrolling=${"no"} >" +
+            "</iframe>"
     }
 }
