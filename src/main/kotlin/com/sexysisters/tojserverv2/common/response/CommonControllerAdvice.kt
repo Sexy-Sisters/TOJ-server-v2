1171e2220ecc9b1a0bc8b0b05031fbf01b9b4f93
leekyukin <leekujin14@gmail.com>
2022-12-22 15:29:35 +0900

    feat: Get current student

--- src/main/kotlin/com/sexysisters/tojserverv2/common/response/CommonControllerAdvice.kt
+++ src/main/kotlin/com/sexysisters/tojserverv2/common/response/CommonControllerAdvice.kt
@@ -14,39 +14,39 @@ import org.springframework.web.bind.annotation.ResponseStatus
 @ControllerAdvice
 class CommonControllerAdvice {
 
-//    @ResponseBody
-//    @ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
-//    @ExceptionHandler(Exception::class)
-//    fun onException(e: Exception) =
-//        CommonResponse.fail(e.message!!, e.message!!)
-    // CommonResponse.fail(ErrorCode.COMMON_SYSTEM_ERROR)
+    @ResponseBody
+    @ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
+    @ExceptionHandler(Exception::class)
+    fun onException(e: Exception) =
+        CommonResponse.fail(e.message!!, e.message!!)
+//     CommonResponse.fail(ErrorCode.COMMON_SYSTEM_ERROR)
 
-//    @ResponseBody
-//    @ResponseStatus(HttpStatus.OK)
-//    @ExceptionHandler(BaseException::class)
-//    fun onBaseException(e: BaseException) =
-//        CommonResponse.fail(e.message!!, e.errorCode.errorMsg)
+    @ResponseBody
+    @ResponseStatus(HttpStatus.OK)
+    @ExceptionHandler(BaseException::class)
+    fun onBaseException(e: BaseException) =
+        CommonResponse.fail(e.message!!, e.errorCode.errorMsg)
 
-//    @ResponseBody
-//    @ResponseStatus(HttpStatus.OK)
-//    @ExceptionHandler(ClientAbortException::class)
-//    fun skipException(e: Exception) =
-//        CommonResponse.fail(ErrorCode.COMMON_SYSTEM_ERROR)
+    @ResponseBody
+    @ResponseStatus(HttpStatus.OK)
+    @ExceptionHandler(ClientAbortException::class)
+    fun skipException(e: Exception) =
+        CommonResponse.fail(ErrorCode.COMMON_SYSTEM_ERROR)
 
-//    @ResponseBody
-//    @ResponseStatus(HttpStatus.BAD_REQUEST)
-//    @ExceptionHandler(MethodArgumentNotValidException::class)
-//    fun methodArgumentNotValidException(e: MethodArgumentNotValidException): CommonResponse<Unit> {
-//        val bindingResult: BindingResult = e.bindingResult
-//        val fe: FieldError? = bindingResult.fieldError
-//
-//        if (fe != null) {
-//            val message = "Request Error ${fe.field} = ${fe.rejectedValue} + (${fe.defaultMessage})"
-//            return CommonResponse.fail(message, ErrorCode.COMMON_INVALID_PARAMETER.name)
-//        } else {
-//            return CommonResponse.fail(
-//                ErrorCode.COMMON_INVALID_PARAMETER.errorMsg, ErrorCode.COMMON_INVALID_PARAMETER.name
-//            )
-//        }
-//    }
+    @ResponseBody
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    @ExceptionHandler(MethodArgumentNotValidException::class)
+    fun methodArgumentNotValidException(e: MethodArgumentNotValidException): CommonResponse<Unit> {
+        val bindingResult: BindingResult = e.bindingResult
+        val fe: FieldError? = bindingResult.fieldError
+
+        if (fe != null) {
+            val message = "Request Error ${fe.field} = ${fe.rejectedValue} + (${fe.defaultMessage})"
+            return CommonResponse.fail(message, ErrorCode.COMMON_INVALID_PARAMETER.name)
+        } else {
+            return CommonResponse.fail(
+                ErrorCode.COMMON_INVALID_PARAMETER.errorMsg, ErrorCode.COMMON_INVALID_PARAMETER.name
+            )
+        }
+    }
 }
